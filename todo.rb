eada7b2b8536a79e454b7df2d7de04a05d774994
Michael Lisjak <michael.lisjak@web.de>
2022-01-07 11:21:56 +0100

    finished 21. marking todos as completed

--- todo.rb
+++ todo.rb
@@ -129,3 +129,16 @@ post "/lists/:list_id/todos/:todo_id/destroy" do
 
   redirect "/lists/#{@list_id}"
 end
+
+# Complete a todo from the list
+post "/lists/:list_id/todos/:todo_id" do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+
+  todo_id = params[:todo_id].to_i
+  is_completed = params[:completed] == "true"
+  @list[:todos][todo_id][:completed] = is_completed
+  session[:success] = "The todo item has been updated."
+
+  redirect "/lists/#{@list_id}"
+end
